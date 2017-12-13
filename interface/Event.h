fee27dcecea56a1d6404cf521670e9f3283f1ec1
Daniel <daniel.edison.marley@cern.ch>
2017-12-13 00:21:30 -0600

    Initial commit of branch with updated comments in Event class

--- interface/Event.h
+++ interface/Event.h
@@ -200,9 +200,9 @@ class Event {
 
     // TTree variables [all possible ones]
     // ************
-    // the following are from root files accessed 
-    //  on 6 November 2017 (N. Eminizer)
-    //  /store/user/eminizer/B2GTTrees_Jun19/
+    // Setup to read EDMntuple format
+    // -- following this example:
+    //    https://github.com/dmajumder/VLQAna
     unsigned int m_array_size = 500;
 
     TTreeReaderValue<float> * m_weight_mc;          // evt_Gen_Weight
